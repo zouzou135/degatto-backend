61238bbca613fe415b610823f7b6ebb0a82e6b59
josephjreije35 <josephjreije35@gmail.com>
2022-12-18 18:01:38 +0100

    c

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
-
+const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var cors = require("cors");
 app.use(cors());
+app.use(fileUpload());
+app.use(express.json());
+
+var file_buffer = null;
+var file = null;
 
 app.post("/process-data", function (req, res) {
+  console.log(file);
   setTimeout(function () {
     res.send({
       success: true,
@@ -16,7 +22,16 @@ app.post("/process-data", function (req, res) {
   }, 3000);
 });
 
-// what port to run server on
+app.post("/upload-file", function (req, res) {
+  file = req.files;
+  file_buffer = file.data;
+  const fileName = file.name;
+  res.send({
+    success: true,
+  });
+});
+
+// what port to run server on`
 app.listen(3001, function () {
   console.log("server started on port 3001");
 });
